26ee5fd7103f0bf572a7109f9f494eab2d66f625
Mondemen <alexandr.coletta@gmail.com>
2023-06-10 23:47:39 +0200

    Update passport version and modernize code

--- examples/login/utils.js
+++ examples/login/utils.js
@@ -1,4 +1,4 @@
-exports.randomString = function(len) {
+export function randomString(len) {
   var buf = []
     , chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
     , charlen = chars.length;
